eb68c47b5789292618107cd16b2580e582dbb9c5
Vinicius Kuwakino <vikuwakino@gmail.com>
2021-04-02 01:15:09 -0300

    CRUD Cuidador

---
+++ backend/routes/cuidador.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+const CuidadorController = require('../controllers/cuidador-controller')
+
+// Registrar cuidador
+router.post('/', CuidadorController.postCuidador)
+
+// Retorna todos os Cuidadores
+router.get('/', CuidadorController.getCuidador)
+
+// Retorna dados do Cuidador pelo ID
+router.get('/:id_cuidador', CuidadorController.getCuidadorById)
+
+// Altera dados do Cuidador
+router.patch('/', CuidadorController.updateCuidador)
+
+// Excluir um Cuidador
+router.delete('/', CuidadorController.deleteCuidador)
+
+module.exports = router
