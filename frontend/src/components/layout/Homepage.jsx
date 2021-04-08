966c503e48b4cc66824cad286c43acba217bc07d
luanvalese <luanhcvalese@gmail.com>
2021-04-07 22:33:59 -0300

    Atualizando layout da home

--- frontend/src/components/layout/Homepage.jsx
+++ frontend/src/components/layout/Homepage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import "./Home.css"
 import SimpleImageSlider from "react-simple-image-slider";
+import { Layout, Card, Typography } from 'antd';
+import Header from '../template/HomeHeader'
+import 'antd/dist/antd.css';
+const { Title } = Typography;
+const { Content, Footer } = Layout;
 
-import Header from '../template/Header'
 
 const images = [
    { url: "https://www.cesarsway.com/wp-content/uploads/2015/06/6-tips-for-mastering-the-dog-walk.jpg" },
@@ -13,39 +17,37 @@ const images = [
    { url: "https://www.encrenquinhas.com.br/images/ap-smart-layerslider/homepage/slide-14.jpg" },
 ];
 
-export default props =>
-   <div className="homepage">
-      <Header
-         link1={
-         <Link to="/usuario">
-            <navbutton className="fa">USUÁRIO</navbutton>
-         </Link>
-      }
-         link2={
-         <Link to="/cuidador">
-            <navbutton className="fa">CUIDADOR</navbutton>
-         </Link>
-      }
-         link3={
-         <Link to="/login">
-            <navbutton className="fa">LOGIN</navbutton>
-         </Link>
-      }     
-      />
-         <div className={"displayslide"} >
-            <SimpleImageSlider
-               width={'100%'}
-               height={400}
-               images={images}
-               slideDuration={0.5}
-               showBullets
-               showNavs
-               useGPUrender
-            />
-         </div>
-         <div className="display-4">A SER PRODUZIDO</div>
-         <hr />
-         <p className="mb-0">COISAS QUE A APLICAÇÃO PODE OFERECER</p>
-         <div className="display-4">EXPERIENCIAS DOS USUÁRIOS</div>
-         
+export default () => (
+   <div>
+      <Header/>
+      <Card style={{ width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.105)'}}>
+         <Content className="site-layout" style={{ padding: '0 200px', marginTop: 110 }}>
+         <div className="site-layout-background" style={{ padding: 10 , minHeight: 380, display: 'flex', justifyContent: 'center'}}>
+            <div className="slider">
+                  <SimpleImageSlider
+                     width="100%"
+                     height="580px"
+                     images={images}
+                     slideDuration={0.5}
+                     showBullets
+                     showNavs
+                     useGPUrender
+                     />
+               </div>
+            </div>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 380, backgroundColor: 'white'}}>
+               <Card class="defaultCard" style={{ marginTop: 10, textAlign: 'center'}}>
+                  <Title type="warning" >VIAJE COM TRANQUILIDADE E DEIXE SEU ANIMALZINHO CONOSCO!</Title>
+               </Card>
+               <Card style={{ marginTop: 10, textAlign: 'center'}}>
+                  <Title type="secondary" >VENHA CONHECER A NANNY PET!</Title>
+               </Card>
+               <Card style={{ marginTop: 10, textAlign: 'center'}}>
+                  <Title type="warning" >SEJA UM NANNY!</Title>
+               </Card>
+            </div>
+         </Content>
+      </Card>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
    </div>
+   )
