b43c89f0dd3607415e34949e0ec019360bf887f5
luanvalese <luanhcvalese@gmail.com>
2021-04-07 22:34:24 -0300

    Atualizando layout do usuário e cuidador

--- frontend/src/components/layout/cuidador/LayoutCuidador.jsx
+++ frontend/src/components/layout/cuidador/LayoutCuidador.jsx
@@ -1,17 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import Header from '../../template/Header'
+import Header from '../../template/UserHeader'
 
-export default props =>
-   <div className="cuidador">
-      <Header
-         link1={
-            <Link to="/">
-               <navbutton className="fa fa-home">
-                  VOLTAR PARA HOME
-               </navbutton>
-            </Link>
-         }
-      />
-      <text>CUIDADOR</text>
-   </div>
+export default () => <Header/>
--- frontend/src/components/layout/user/LayoutUser.jsx
+++ frontend/src/components/layout/user/LayoutUser.jsx
@@ -1,21 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Header from '../../template/UserHeader'
 
-import Header from '../../template/Header'
-import Footer from '../../template/Footer'
-
-
-export default props =>
-   <div className="usuario">
-
-      <Header link1={
-         <Link to="/">
-            <navbutton className="fa fa-home">
-               VOLTAR PARA HOME
-            </navbutton>
-         </Link>
-      }
-      />
-      <text>USUÁRIO</text>
-
-   </div>
+export default () => <Header/>
