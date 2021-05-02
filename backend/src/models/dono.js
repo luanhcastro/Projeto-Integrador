c24428952d674f097f8ebc4ce443e75bee0cc24e
Vinicius Kuwakino <vikuwakino@gmail.com>
2021-05-02 20:51:48 -0300

    Model de Donos

---
+++ backend/src/models/dono.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize')
+
+class Dono extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: DataTypes.STRING,
+      dataNascimento: DataTypes.DATE,
+      cpf: DataTypes.STRING,
+      endereco: DataTypes.STRING,
+      telefone: DataTypes.STRING
+    }, {
+      // Conexao com o banco de dados
+      sequelize,
+      tableName: 'dono'
+    })
+  }
+
+  static associate(models) {
+    // Donos tem muitos pets
+    this.hasMany(models.Pet, { foreignKey: 'idDono', as: 'pet'})
+  }
+}
+
+module.exports = Dono
