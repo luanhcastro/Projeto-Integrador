2fb5bcc95fae45f9f3c072a0270eafe288f56699
Bianca Miazaki <bmiazaki@gmail.com>
2021-05-03 16:23:48 -0300

    Add files via upload

---
+++ backend/src/controllers/pet-controller.js
@@ -0,0 +1,84 @@
+const Dono = require('../models/dono')
+const Pet = require('../models/pet')
+
+module.exports = {
+
+  // Create Pet
+  async postPet(req, res) {
+    const {
+      nomePet,
+      raca,
+      porte,
+      idade,
+      idDono,
+    } = req.body
+
+    const dono = await Dono.findByPk(idDono)
+
+    if (!dono) return res.status(400).json({ error: 'Dono nao existe' })
+
+    const pet = await Pet.create({
+      nomePet,
+      raca,
+      porte,
+      idade,
+      idDono,
+    })
+
+    return res.json(pet)
+  },
+
+  // Get Pets
+  async getPet(req, res) {
+    const pets = await Pet.findAll()
+
+    return res.json(pets)
+  },
+
+  // Get Pet By Id
+  async getPetById(req, res) {
+    const { idPet } = req.params
+    const pet = await Pet.findByPk(idPet, {
+      include: { association: 'dono' }
+    })
+
+    return res.json(pet)
+  },
+
+  // Update Pet
+  async updatePet(req, res) {
+    const {
+      idPet,
+      nomePet,
+      raca,
+      porte,
+      idade,
+    } = req.body
+
+    const pet = await Pet.update({
+      nomePet,
+      raca,
+      porte,
+      idade,
+    }, {
+      where: {
+        id: idPet
+      }
+    })
+
+    return res.json(pet)
+  },
+
+  // Delete Pet
+  async deletePet(req, res) {
+    const { idPet } = req.body
+    const pet = await Pet.destroy({
+      where: {
+        id: idPet
+      }
+    })
+
+    return res.json(pet)
+  }
+
+}
