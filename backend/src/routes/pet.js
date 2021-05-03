eddc347fff9ea2f06c87ac36d89c4b2386fdef27
Bianca Miazaki <bmiazaki@gmail.com>
2021-05-03 16:20:01 -0300

    Add files via upload

---
+++ backend/src/routes/pet.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+const PetController = require('../controllers/pet-controller')
+
+// Registra Pet
+router.post('/', PetController.postPet)
+
+// Retorna todos os Pets
+router.get('/', PetController.getPet)
+
+// Retorna todos do Pet pelo ID
+router.get('/:idPet', PetController.getPetById)
+
+// Altera dados do Pet
+router.patch('/', PetController.updatePet)
+
+// Exclui um Pet
+router.delete('/', PetController.deletePet)
+
+module.exports = router
