e556c8a02caf99703489fcd637b1758587c67808
Bianca Miazaki <bmiazaki@gmail.com>
2021-05-24 18:55:08 -0300

    Add files via upload

--- backend/src/__tests__/dono.test.js
+++ backend/src/__tests__/dono.test.js
@@ -1,24 +1,74 @@
 const request = require('supertest')
 const app = require('../../app')
 
-
 describe('TDD de CRUD de Donos', () => {
   let UserID
+  let Token
+
   it('Teste: Deve criar um Dono', async (done) => {
     const response = await request(app)
     .post('/dono')
     .send({
-      nome: "Vinicius Kuwakino",
+      nome: "Bianca Miazaki",
       senha: "1234",
-      email: "vinicius@hotmail.com",
-      dataNascimento: "1999-05-30",
+      email: "bianca@hotmail.com",
+      dataNascimento: "1999-04-15",
       cpf: "12345678960",
       endereco: "Rua 2, 222",
-      telefone: 44999990006
+      telefone: "44999990006"
     })
     
     UserID = response.body.id
+    // console.log(response.body)
+
     expect(response.status).toBe(200)
+    done()
+  })
+
+  it('Teste: Deve dar erro ao criar um Dono', async (done) => {
+    const response = await request(app)
+    .post('/dono')
+    .send({
+      nome: "Vinicius Kuwakino",
+      senha: "12341234",
+      email: "vinicius@hotmail.com",
+      dataNascimento: "1999-04-15",
+      cpf: "12345678960",   // cpf ja existe
+      endereco: "Rua 2, 222",
+      telefone: "44999990007"
+    })
+    
+    // console.log(response.body)
+
+    expect(response.status).toBe(400)
+    done()
+  })
+
+  it('Teste: Deve efetuar o login do Dono', async (done) => {
+    const response = await request(app)
+    .post('/dono/loginDono')
+    .send({
+      email: "bianca@hotmail.com",
+      senha: "1234",
+    })
+
+    Token = response.body.token
+    
+    console.log(Token)
+
+    expect(response.status).toBe(200)
+    done()
+  })
+
+  it('Teste: Deve dar erro ao efetuar o login do Dono', async (done) => {
+    const response = await request(app)
+    .post('/dono/loginDono')
+    .send({
+      email: "bianca741@hotmail.com",
+      senha: "1234abcd",
+    })
+
+    expect(response.status).toBe(400)
     done()
   })
   
@@ -26,6 +76,8 @@ describe('TDD de CRUD de Donos', () => {
     const response = await request(app)
       .get('/dono')
 
+    // console.log(response.body)
+    
     expect(response.status).toBe(200)
     done()
   })
@@ -34,34 +86,42 @@ describe('TDD de CRUD de Donos', () => {
     const response = await request(app)
       .get('/dono/' + UserID)
 
+    // console.log(response.body)
+
     expect(response.status).toBe(200)
     done()
   })
 
-  it('Teste: Deve alterar os dados de um Dono', async (done) => {
+  it('Teste: Deve alterar os dados deste Dono', async (done) => {
     const response = await request(app)
       .patch('/dono')
+      .set('Authorization', `Bearer ${Token}`)
       .send({
         id: UserID,
-        email: "vinicius@hotmail.com",
-        nome: "Vinicius Japones Kuwakino",
-        dataNascimento: "1999-05-30",
-        cpf: "12345678960",
-        endereco: "Rua 1, 111",
-        telefone: 44999990006
+        nome: "Bianca Miazaki Japanese",
+        senha: "1234",
+        email: "biancaMiazaki@hotmail.com",
+        dataNascimento: "1999-04-15",
+        endereco: "Rua 3, 321",
+        telefone: "44999990006"
       })
-      
+    
+    // console.log(response.body)
+
     expect(response.status).toBe(200)
     done()
   })
 
   it('Teste: Deve deletar um Dono dado o seu respectivo ID', async (done) => {
     const response = await request(app)
-      .del('/dono')
-      .send({
-        idDono: UserID
-      })
+    .del('/dono')
+    .set('Authorization', `Bearer ${Token}`)
+    .send({
+      idDono: UserID
+    })
 
+    // console.log(response.body)
+    
     expect(response.status).toBe(200)
     done()
   })
