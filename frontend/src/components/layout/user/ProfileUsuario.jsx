db30a59a0e1cad3b3939ab002d84d35b8456badb
luanvalese <luanhcvalese@gmail.com>
2021-05-25 13:45:47 -0300

    crud usuário

--- frontend/src/components/layout/user/ProfileUsuario.jsx
+++ frontend/src/components/layout/user/ProfileUsuario.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import "../Home.css"
 import { Layout, Card, Typography, Form, Input, notification, Button, Row, Col, Alert } from 'antd';
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
@@ -14,44 +14,78 @@ const { Title } = Typography;
 const { Content, Footer, Header } = Layout;
 
 
-const AdvancedSearchForm = () => {
+const ProfileUser = () => {
    const [form] = Form.useForm();
-   const [showAlert, setShowAlert] = useState(false);
-   const [showErrorAlert, setShowErrorAlert] = useState(false);
-   const url = 'http://localhost:3001/profileuser'
-   const onFinish = (values) => {
-      console.log('Received values of form: ', values);
-      axios.post(url,
+   const [loading, setLoading] = useState(false);
+   const idDono = localStorage.getItem('id')
+   const getUrl = `http://localhost:3001/dono/${idDono}`
+   const patchUrl = 'http://localhost:3001/dono/'
+
+
+   const formataData = data => {
+      var date = new Date()
+      var day = date.getDate();
+      var month = date.getMonth();
+      var year = date.getFullYear();
+      return year + '-' + (month++) + '-' + day;
+   }
+
+   const request = () => {
+      axios.get(getUrl)
+         .then((response) => {
+            console.log(response)
+            form.setFieldsValue({
+               id: response.data.id,
+               nome: response.data.nome,
+               dataNasc: formataData(response.data.dataNascimento),
+               cpf: response.data.cpf,
+               email: response.data.email,
+               senha: response.data.senha,
+               endereco: response.data.endereco,
+               qtdPetsdono: response.data.pet.length,
+               telefone: response.data.telefone,
+            });
+         })
+         .catch((err) => {
+            console.log(err);
+         })
+         .finally(() => setLoading(false))
+   };
+   const onFinish = async (values) => {
+      await axios.patch(patchUrl,
          {
+            id: idDono,
             nome: values.nome,
             dataNascimento: values.dataNasc,
             cpf: values.cpf,
-            endereco: values.endereco,
-            numServicos: 0,
             email: values.email,
             senha: values.senha,
+            endereco: values.endereco,
+            qtdPetsdono: values.qtdPetsdono,
             telefone: values.telefone,
          })
-         .then((response) => {
+         .then(() => {
             notification['success']({
-               message: 'Cadastrado com sucesso',
+               message: 'Atualizado com sucesso',
                description:
-                  'Parabéns, agora é só logar na nossa plataforma com seu email e senha!',
+                  'Seus dados foram atualizados',
             });
-            console.log('122232312');
-            form.resetFields();
-            console.log(response);
          })
-         .then(() => console.log('alert', showAlert))
          .catch((err) => {
             notification['error']({
-               message: 'Não foi possivel cadastrar',
+               message: 'Não foi possivel atualizar',
                description:
                   'Verifique se seus dados estão corretos.',
             });
             console.log(err);
          })
-   }
+   };
+
+   useEffect(() => {
+      setLoading(true);
+      request();
+   }, []);
+
 
 
 
@@ -72,12 +106,164 @@ const AdvancedSearchForm = () => {
          <Card style={{ width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.105)' }}>
             <Content className="site-layout" style={{ padding: '0 200px', minHeight: '800px', marginTop: 110 }}>
                <div className="site-layout-background" style={{ padding: 10, minHeight: 380 }}>
-                  <Card style={{ textAlign: 'center', minHeight: '800px' }} title={<Title type="warning">PERFIL DE CLIENTE</Title>}>
-                     {showAlert && (<Alert message='Cadastro salvo com sucesso!' type='success' showIcon closable />)}
-                     {showErrorAlert && (<Alert message='Não foi possível efetuar o cadastro' type='error' showIcon closable />)}
+                  <Card style={{ textAlign: 'center', minHeight: '800px' }} loading={loading} title={<Title type="warning">PERFIL</Title>}>
+                     <Form
+                        style={{ marginTop: 50 }}
+                        form={form}
+                        name="advanced_search"
+                        layout="vertical"
+                        loading={loading}
+                        className="ant-advanced-search-form"
+                        onFinish={onFinish}
+                        size="large"
+                     >
+                        <Row gutter={24}>
+                           <Col span={24}>
+                              <Form.Item
+                                 name="nome"
+                                 label="Nome"
+                                 rules={[
+                                    {
+                                       required: true,
+                                       message: 'Preencha o campo!',
+                                    },
+                                 ]}
+                              >
+                                 <Input
+                                    maxLength={50}
+                                    placeholder="Insira seu nome" />
+                              </Form.Item>
+                           </Col>
+                           <Col span={12}>
+                              <Form.Item
+                                 name="dataNasc"
+                                 label="Data de Nascimento"
+                                 rules={[
+                                    {
+                                       required: true,
+                                       message: 'Preencha o campo!',
+                                    },
+                                 ]}
+                              >
+                                 <Input
+                                    maxLength={11}
+                                    placeholder="Insira sua data de Nascimento" />
+                              </Form.Item>
+                              <Form.Item
+                                 name="qtdPetsdono"
+                                 label="Quantidade de Pets"
+                                 rules={[
+                                    {
+                                       required: true,
+                                       message: 'Preencha o campo!',
+                                    },
+                                 ]}
+                              >
+                                 <Input
+                                    maxLength={11}
+                                    placeholder="Insira quantos pets você possui" />
+                              </Form.Item>
+                           </Col>
+                           <Col span={12}>
+                              <Form.Item
+                                 name="cpf"
+                                 label="CPF"
+                                 rules={[
+                                    {
+                                       required: true,
+                                       message: 'Preencha o campo!',
+                                    },
+                                 ]}
+                              >
+                                 <Input
+                                    maxLength={11}
+                                    placeholder="Insira seu CPF"
+                                 />
+                              </Form.Item>
+                              <Form.Item
+                                 name="telefone"
+                                 label="Telefone/Celular"
+                                 rules={[
+                                    {
+                                       required: true,
+                                       message: 'Preencha o campo!',
+                                    },
+                                 ]}
+                              >
+                                 <Input
+                                    maxLength={12}
+                                    placeholder="Insira seu telefone ou celular"
+                                 />
+                              </Form.Item>
+                           </Col>
+                           <Col span={24}>
+                              <Form.Item
+                                 name="endereco"
+                                 label="Endereço"
+                                 rules={[
+                                    {
+                                       required: true,
+                                       message: 'Preencha o campo!',
+                                    },
+                                 ]}
+                              >
+                                 <Input
+                                    maxLength={50}
+                                    placeholder="Insira seu endereço"
+                                 />
+                              </Form.Item>
+                           </Col>
+                           <Col span={12}>
+                              <Form.Item
+                                 name="email"
+                                 label="Email"
+                                 rules={[
+                                    {
+                                       required: true,
+                                       message: 'Preencha o campo!',
+                                    },
+                                 ]}
+                              >
+                                 <Input
+                                    placeholder="Insira seu email"
+                                 />
+                              </Form.Item>
+                           </Col>
+                           <Col span={12}>
+                              <Form.Item
+                                 name="senha"
+                                 label="Senha"
+                                 rules={[
+                                    {
+                                       required: true,
+                                       message: 'Preencha o campo!',
+                                    },
+                                 ]}
+                              >
+                                 <Input.Password
+                                    placeholder="Insira uma senha"
+                                    iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                                 />
+                              </Form.Item>
+                           </Col>
+                        </Row>
+                        <Row style={{ marginTop: '200px' }}>
+                           <Col span={24} style={{ textAlign: 'right' }}>
+                              <Button
+                                 style={{ margin: '0 8px' }}
+                                 onClick={() => {
+                                    form.resetFields();
+                                 }}
+                              >
+                                 Limpar campos
+                  </Button>
 
-
-
+                              <Button type="primary" htmlType="submit">
+                                 Enviar
+                      </Button>
+                           </Col>
+                        </Row>
+                     </Form>
                   </Card>
                </div>
             </Content>
@@ -86,4 +272,4 @@ const AdvancedSearchForm = () => {
       </div>
    )
 }
-export default AdvancedSearchForm;
+export default ProfileUser;
