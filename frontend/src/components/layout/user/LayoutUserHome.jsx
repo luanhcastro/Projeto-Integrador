8efd5f9a09d841ea2aef8ec92c9ccf41d455dc93
luanvalese <luanhcvalese@gmail.com>
2021-05-25 03:11:30 -0300

    listagem de pets

--- frontend/src/components/layout/user/LayoutUserHome.jsx
+++ frontend/src/components/layout/user/LayoutUserHome.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "../Home.css"
-import { Layout, Card, Typography, Form, Input, Button, Row, Col, notification } from 'antd';
-import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import { Layout, Card, Typography, Button, Row, notification, Tag, Space, Table } from 'antd';
 import 'antd/dist/antd.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -13,11 +12,106 @@ import { LogoutOutlined } from '@ant-design/icons';
 const { Title } = Typography;
 const { Content, Footer, Header } = Layout;
 
-function handleLogout() {
-
-};
 
 const User = () => {
+
+  const userId = localStorage.getItem('id');
+  const [loading, setLoading] = useState(false);
+  const [tableData, setTableData] = useState([]);
+
+  const getUrl = `http://localhost:3001/dono/${userId}`
+  const deleteUrl = 'http://localhost:3001/pet'
+  const request = () => {
+    axios.get(getUrl)
+      .then(response => {
+        console.log(response.data.pet)
+
+        setTableData(response.data.pet.map((pet, index) => ({
+          key: pet.id,
+          nome: pet.nomePet,
+          raca: pet.raca,
+          porte: pet.porte,
+        }))
+        )
+      })
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((err) => {
+        notification['error']({
+          message: 'Não foi possivel realizar o login',
+          description:
+            'Verifique se seus dados estão corretos.',
+        });
+        console.log(err);
+      })
+  }
+
+  const deletePet = async id => {
+    console.log(111111, id);
+    await axios.delete(deleteUrl,
+      {
+        idPet: id,
+      })
+      .then(response => {
+        console.log('deletado', response);
+      })
+      .catch((err) => {
+        notification['error']({
+          message: 'Erro!',
+          description:
+            'Não foi possível deletar seu pet.',
+        });
+        console.log(err);
+      })
+  }
+  const redirectEdit = id => {
+    localStorage.setItem('currentPetId', id);
+    window.location = 'pet#/homeUsuario/pets';
+  }
+  useEffect(() => {
+    setLoading(true);
+    request();
+  }, []);
+
+  useEffect(() => {
+    console.log(222, tableData)
+  }, [tableData]);
+
+
+
+
+
+
+  const columns = [
+    {
+      title: 'Nome',
+      dataIndex: 'nome',
+      key: 'nome',
+      render: text => <a>{text}</a>,
+    },
+    {
+      title: 'Raça',
+      dataIndex: 'raca',
+      key: 'raca',
+    },
+    {
+      title: 'Porte',
+      key: 'porte',
+      dataIndex: 'porte',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <Button type="ghost" onClick={() => redirectEdit(record.key)}>Editar</Button>
+          <Button type="danger" onClick={() => deletePet(record.key)}>Deletar</Button>
+        </Space>
+      ),
+    },
+  ];
+
 
   return (
     <div>
@@ -26,32 +120,36 @@ const User = () => {
           <img src={logo} alt='logo' class="logo" />
         </Link>
         <div theme="light" mode="horizontal" class="buttons" >
-          <Button type="primary" size="large" style={{ margin: 10, backgroundColor: 'red' }}
-            icon={(<LogoutOutlined style={{ fontSize: 20 }}
-              onPress={handleLogout}
-            />)}>LOGOUT</Button>
+          <Link to="/">
+            <Button type="danger" size="large" style={{ margin: 10 }}
+              icon={(<LogoutOutlined style={{ fontSize: 20 }}
+              />)}>LOGOUT</Button>
+          </Link>
         </div>
       </Header>
       <Card style={{ width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.105)' }}>
         <Content className="site-layout" style={{ padding: '0 200px', minHeight: '800px', marginTop: 110 }}>
           <div className="site-layout-background" style={{ padding: 10, minHeight: 380 }}>
-            <Card style={{ textAlign: 'center', minHeight: '800px' }} title={<Title type="warning">HOMEPAGE CLIENTE</Title>}>
+            <Card style={{ textAlign: 'center', minHeight: '800px' }} title={<Title type="warning">MEUS PETS</Title>}>
 
-              <Link to='/homeUsuario/profile'>
-                <Button >
-                  TESTE NAVEGACAO PERFIL USER
+              <Row style={{ display: 'flex', justifyContent: 'space-around', marginTop: 20 }}>
+                <Link to='/homeUsuario/profile'>
+                  <Button style={{minWidth: 180}} >
+                    PERFIL
                         </Button>
-              </Link>
-              <Link to='/homeUsuario/pets'>
-                <Button >
-                  NAVEGACAO PETS CRUD
+                </Link>
+                <Link to='/homeUsuario/pets'>
+                  <Button style={{minWidth: 180}} >
+                    CADASTRAR NOVO PET
                         </Button>
-              </Link>
-              <Link to='/homeUsuario/buscaCuidador'>
-                <Button >
-                  NAVEGACAO BUSCA
+                </Link>
+                <Link to='/homeUsuario/buscaCuidador'>
+                  <Button style={{minWidth: 180}} >
+                    BUSCAR CUIDADOR
                         </Button>
-              </Link>
+                </Link>
+              </Row>
+              <Table style={{marginTop: 30}}columns={columns} loading={loading} dataSource={tableData} />
 
             </Card>
           </div>
